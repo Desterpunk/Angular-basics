a67839503f22260b29f59921dc806e6f381eb5db
Jhon Serna Durango <jhon.serna@sofka.com.co>
2022-01-13 12:53:21 -0500

    Obtener informacion de la ruta

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.css']
 })
 export class FirstComponent implements OnInit {
+  name:string = "";
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.name = params['name'];
+      console.log(params)
+    });
   }
 
 }

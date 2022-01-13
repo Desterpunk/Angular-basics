bcc0349d865cd1e6ae76a6637508f21414a77964
Jhon Serna Durango <jhon.serna@sofka.com.co>
2022-01-13 13:13:57 -0500

    Rutas de anidamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { ChildBComponent } from './child-b/child-b.component';
+import { ChildAComponent } from './child-a/child-a.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
@@ -5,8 +7,22 @@ import { SecondComponent } from './second/second.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'first-component', component: FirstComponent },
+  {
+    path: 'first-component',
+    component: FirstComponent, // this is the component with the <router-outlet> in the template
+    children: [
+      {
+        path: 'child-a', // child route path
+        component: ChildAComponent, // child route component that the router renders
+      },
+      {
+        path: 'child-b',
+        component: ChildBComponent, // another child route component that the router renders
+      },
+    ],
+  },
   { path: 'second-component', component: SecondComponent },
+  { path: '',   redirectTo: '/first-component', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
 
